49fd64fb99c2aaf3fbe30487648daa21f9de84b5
binsong.li <binsong.li@tongdun.cn>
2022-11-10 23:31:24 +0800

    fix checkstyle

--- jobserver-common/src/main/java/io/github/melin/spark/jobserver/core/entity/SparkDriver.java
+++ jobserver-common/src/main/java/io/github/melin/spark/jobserver/core/entity/SparkDriver.java
@@ -9,7 +9,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.Formula;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
