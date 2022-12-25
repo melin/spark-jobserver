071fb350ae6731fb191a1daada61ab502853791e
binsong.li <binsong.li@tongdun.cn>
2022-12-25 19:34:39 +0800

    add Transactional

--- jobserver-driver/src/main/java/io/github/melin/spark/jobserver/driver/SparkDriverContext.java
+++ jobserver-driver/src/main/java/io/github/melin/spark/jobserver/driver/SparkDriverContext.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 
@@ -43,6 +44,7 @@ public class SparkDriverContext {
     @Autowired
     private TaskLogThread logThread;
 
+    @Transactional
     public void initSparkDriver(Long driverId) {
         SparkDriverEnv.getSparkContext().addSparkListener(sparkListener);
         SparkDriver driver = driverService.getEntity(driverId);
