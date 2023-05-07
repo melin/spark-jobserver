7f493ea132b4e1b661129768b1316c0b87332a00
melin <libinsong1204@gmail.com>
2023-05-07 17:41:51 +0800

    add common vfs demo

--- jobserver-extensions/src/main/java/com/github/melin/jobserver/extensions/VfsS3Demo.java
+++ jobserver-extensions/src/main/java/com/github/melin/jobserver/extensions/VfsS3Demo.java
@@ -1,6 +1,8 @@
 package com.github.melin.jobserver.extensions;
 
 import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.github.vfss3.S3FileSystemConfigBuilder;
 import org.apache.commons.vfs2.*;
 
@@ -13,13 +15,17 @@ public class VfsS3Demo {
 
         FileSystemOptions options = new FileSystemOptions();
         S3FileSystemConfigBuilder.getInstance().setUseHttps(options, false);
+
+        BasicAWSCredentials credentials = new BasicAWSCredentials("BxiljVd5YZa3mRUn", "3Mq9dsmdMbN1JipE1TlOF7OuDkuYBYpe");
+        AWSStaticCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(credentials);
+        S3FileSystemConfigBuilder.getInstance().setCredentialsProvider(options, credentialsProvider);
         ClientConfiguration clientConfiguration = new ClientConfiguration();
         clientConfiguration.setMaxErrorRetry(3);
         S3FileSystemConfigBuilder.getInstance().setClientConfiguration(options, clientConfiguration);
 
-        FileObject fileObject = fsManager.resolveFile("s3://BxiljVd5YZa3mRUn:3Mq9dsmdMbN1JipE1TlOF7OuDkuYBYpe@cdh1:9300/demo-bucket/demo.csv", options);
-        System.out.println(fileObject.getName().getPath());
-        System.out.println(fileObject.getContent().getString(StandardCharsets.UTF_8));
+        //FileObject fileObject = fsManager.resolveFile("s3://BxiljVd5YZa3mRUn:3Mq9dsmdMbN1JipE1TlOF7OuDkuYBYpe@cdh1:9300/demo-bucket/demo.csv", options);
+        //System.out.println(fileObject.getName().getPath());
+        //System.out.println(fileObject.getContent().getString(StandardCharsets.UTF_8));
 
         //fsManager.closeFileSystem(fileObject.getFileSystem());
 
@@ -30,7 +36,7 @@ public class VfsS3Demo {
             System.out.println(fo.getContent().getString(StandardCharsets.UTF_8));
         }
 
-        fsManager.closeFileSystem(fileObject.getFileSystem());
+        fsManager.closeFileSystem(fileObject1.getFileSystem());
     }
 
 }
--- jobserver-extensions/src/main/java/com/github/melin/jobserver/extensions/vfs/FileSystemUtils.java
+++ jobserver-extensions/src/main/java/com/github/melin/jobserver/extensions/vfs/FileSystemUtils.java
@@ -1,6 +1,8 @@
 package com.github.melin.jobserver.extensions.vfs;
 
 import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.github.vfss3.S3FileSystemConfigBuilder;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.vfs2.FileSystemOptions;
@@ -29,10 +31,16 @@ public class FileSystemUtils {
         if (StringUtils.contains(pathStr, "ftp:/")) {
             FtpFileSystemConfigBuilder.getInstance().setPassiveMode(options, true);
         } else if (StringUtils.contains(pathStr, "sftp:/")) {
-
             SftpFileSystemConfigBuilder.getInstance().setIdentities(options, new File[0]);
-        } else if (StringUtils.contains(pathStr, "sftp:/")) {
+        } else if (StringUtils.contains(pathStr, "s3:/")) {
             S3FileSystemConfigBuilder.getInstance().setUseHttps(options, false);
+
+            String userInfo = StringUtils.substringBetween(pathStr, "s3:/", "@");
+            String[] items = StringUtils.split(userInfo, ":");
+            BasicAWSCredentials credentials = new BasicAWSCredentials(items[0], items[1]);
+            AWSStaticCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(credentials);
+            S3FileSystemConfigBuilder.getInstance().setCredentialsProvider(options, credentialsProvider);
+
             ClientConfiguration clientConfiguration = new ClientConfiguration();
             clientConfiguration.setMaxErrorRetry(3);
             S3FileSystemConfigBuilder.getInstance().setClientConfiguration(options, clientConfiguration);
